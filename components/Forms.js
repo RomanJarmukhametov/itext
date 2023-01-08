e3cb5bceb418869f402b68805ffa7281611fe89b
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2023-01-08 13:55:20 +0600

    vue.js forms

--- components/Forms.js
+++ components/Forms.js
@@ -44,20 +44,20 @@ app.component("quote-form", {
                     <p> {{ language.formParagraph }} </p>
 
                   
-
-                   <form novalidate @submit.prevent="validateForm" id="my-form" action="https://formspree.io/f/xdojvazp" enctype="multipart/form-data" method="post">
+                 <form action="https://formspree.io/f/xdojvazp" enctype="multipart/form-data" method="post">
                         <div class="form-row mb-3">
                             <div class="col">
                                 <label for="name" class="form-label"> {{ language.formLabelName }} </label><br>
-                                <input type="text" class="form-control" id="name" name="Client name" v-model="name" :class="{ 'is-invalid': errors.name }" required>
-                                <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
+                                <input type="text" class="form-control" id="name" name="Client name" v-model="name" :class="{ 'is-invalid': nameError }" required>
+        
+                              <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
                             </div>
                         </div>
 
                         <div class="form-row mb-3">
                             <div class="col">
                                 <label for="email" class="form-label"> {{ language.formLabelEmail }} </label>
-                                <input type="email" class="form-control" id="email" name="email" v-model="email" :class="{ 'is-invalid': errors.email }" required>
+                                <input type="email" class="form-control" id="email" name="email" v-model="email" :class="{ 'is-invalid': emailError }" required>
                                 <div v-if="errors.email" class="invalid-feedback">{{ errors.email }}</div>
                             </div>
                         </div>
@@ -65,7 +65,7 @@ app.component("quote-form", {
                         <div class="form-row mb-3">
                             <div class="col">
                                 <label for="phone" class="form-label"> {{ language.formLabelPhone }} </label>
-                                <input type="tel" class="form-control" id="phone" name="phone" v-model="phone" :class="{ 'is-invalid': errors.phone }" required>
+                                <input type="tel" class="form-control" id="phone" name="phone" v-model="phone" :class="{ 'is-invalid': phoneError }" required>
                                 <div v-if="errors.phone" class="invalid-feedback">{{ errors.phone }}</div>
                             </div>
                         </div>
@@ -74,7 +74,7 @@ app.component("quote-form", {
                         <div class="form-row mb-3">
                             <div class="col">
                             <label for="source-language" class="form-label"> {{ language.formLabelSourceLanguage }} </label>
-                            <select class="form-select" ref="source-language" id="source-language" name="Source language" :class="{ 'is-invalid': errors.sourceLanguage }" required @change="validateSourceLanguage">
+                            <select class="form-select" id="source-language" name="Source language">
                                 <option selected disabled value=""> {{ language.formLabelSourceLanguageSelect }} </option>
                                 <option v-for="item in language.translationLanguages">{{ item }}</option>
                             </select>
@@ -85,7 +85,7 @@ app.component("quote-form", {
                          <div class="form-row mb-3">
                             <div class="col">
                             <label for="target-language" class="form-label"> {{ language.formLabelTargetLanguage }} </label>
-                            <select class="form-select" ref="target-language" id="target-language" name="Target language" :class="{ 'is-invalid': errors.targetLanguage }" required @change="validateTargetLanguage">
+                            <select class="form-select" id="target-language" name="Target language">
                                 <option selected disabled value=""> {{ language.formLabelTargetLanguageSelect }} </option>
                                 <option v-for="item in language.translationLanguages">{{ item }}</option>
                             </select>
@@ -123,21 +123,18 @@ app.component("quote-form", {
                             </div>
                         </div>
 
-                        <!-- reCaptcha 
+                        <!-- reCaptcha -->
                         <div class="g-recaptcha mb-3" data-sitekey="6Lc0VVEeAAAAALpm1UU4pLQlLLfJZiAK-tNGKV90"></div> 
 
-                        <input type="hidden" name="_language" value="ru" /> -->
+                        <input type="hidden" name="_language" value="ru" /> 
 
-                        <!-- honeypot field 
-                        <input type="text" name="_gotcha" style="display:none" /> -->
+                        <!-- honeypot field -->
+                        <input type="text" name="_gotcha" style="display:none" /> 
 
                         <div class="form-row mb-3">
                             <div class="col">
-                                <button class="btn btn-primary w-100" type="submit"> {{ language.buttonText }} </button>
+                                <button class="btn btn-primary w-100" type="submit" v-bind:disabled="!name || !email || !phone"> {{ language.buttonText }} </button>
                             </div>
-
-                            <p id="my-form-status"></p>
-
                         </div>
                     </form>
                 </div>
@@ -166,9 +163,9 @@ app.component("quote-form", {
         formLabelName: "Ваше имя (обязательно)",
         formLabelEmail: "Ваш email (обязательно)",
         formLabelPhone: "Ваш номер телефона (обязательно)",
-        formLabelSourceLanguage: "Язык оригинала (обязательно)",
+        formLabelSourceLanguage: "Язык оригинала",
         formLabelSourceLanguageSelect: "Выберите язык оригинала",
-        formLabelTargetLanguage: "Язык перевода (обязательно)",
+        formLabelTargetLanguage: "Язык перевода",
         formLabelTargetLanguageSelect: "Выберите язык перевода",
         translationLanguages: [
           "Казахский язык",
@@ -204,188 +201,227 @@ app.component("quote-form", {
         formLabelComment: "Ваш комментарий к заказу",
         buttonText: "Заказать перевод",
       },
+
+      kazakhText: {
+        titleText: "Кәсіби аударма - бұл",
+        titleSpanText: "қарапайым және оңай",
+        titleParagraph:
+          "Біздің 45 аудармашымыз 2012 жылдан бері 1300-ден астам клиентке кәсіби аудармалар жасайды",
+        officeSection: "Біздің кеңсемізге келіңіз",
+        officeSectionSchedule: "Жұмыс уақыты:",
+        officeSectionWorkingHours:
+          "Дүйсенбі - жұма: 9.30 - 17.00, үзіліс: 12.00 - 14.00",
+        officeSectionWeekend: "Сенбі - жексенбі: демалыс күндері",
+        officeSectionAddressTitle: "Мекенжай",
+        officeSectionAddress: "Жеңіс алаңы 25, 201 кеңсе",
+        emailText:
+          "mailto:itext@itext.kz?subject=Веб-сайттан аудармаға тапсырыс беру&body=Қайырлы күн! Қосымша құжаттардың аудармасы қажет",
+        formTitle: "Аударымға тапсырыс беру нысаны",
+        formParagraph:
+          "Тапсырыс беру нысанын толтырыңыз, біз жақын арада жауап береміз.",
+        formLabelName: "Сіздің есіміңіз (міндетті)",
+        formLabelEmail: "Сіздің email-ңіз (міндетті)",
+        formLabelPhone: "Сіздің телефон нөміріңіз (міндетті)",
+        formLabelSourceLanguage: "Түпнұсқа тілі",
+        formLabelSourceLanguageSelect: "Түпнұсқа тілін таңдаңыз",
+        formLabelTargetLanguage: "Аударма тілі",
+        formLabelTargetLanguageSelect: "Аударма тілін таңдаңыз",
+        translationLanguages: [
+          "Қазақ тілі",
+          "Ағылшын тілі",
+          "Орыс тілі",
+          "Неміс тілі",
+          "Болгар тілі",
+          "Мажар тілі",
+          "Грек тілі",
+          "Дат тілі",
+          "Испан тілі",
+          "Италиян тілі",
+          "Латыш тілі",
+          "Литва тілі",
+          "Голланд тілі",
+          "Поляк тілі",
+          "Португал тілі",
+          "Румын тілі",
+          "Словак тілі",
+          "Словен тілі",
+          "Фин тілі",
+          "Француз тілі",
+          "Чех тілі",
+          "Швед тілі",
+          "Эстон тілі",
+          "Түрік тілі",
+          "Қытай тілі",
+        ],
+        formLabelFilesSelect: "Аудару үшін файлдарды таңдаңыз",
+        formLabelCertification: "Маған нотариалды растау қажет",
+        formLabelCertificationText:
+          "Нотариалды куәландыру үшін келесі шарттар талап етіледі: 1) Сізге құжаттың түпнұсқасын мына мекенжайдағы кеңсеге әкелу қажет: Жеңіс алаңы 25, офис 201, Павлодар, Қазақстан. 2) егер құжат ТМД елдерінен тыс жерде берілсе, онда апостиль (мысалы, Германияда берілген құжат) немесе консулдық заңдастыру мөрі (мысалы, Қытайда немесе БАӘ-де берілген құжат) болуы керек.",
+        formLabelComment: "Сіздің тапсырысыңызға түсініктеме",
+        buttonText: "Аудармаға тапсырыс беру",
+      },
+
+      englishText: {
+        titleText: "Professional translation is",
+        titleSpanText: "simple and easy",
+        titleParagraph:
+          "Our 45 specialists have been providing professional translations for more than 1,300 clients since 2012",
+        officeSection: "Visit Our Office",
+        officeSectionSchedule: "Office schedule:",
+        officeSectionWorkingHours:
+          "Mon-Fri: 9.30 - 17.00, break: 12.00 - 14.00",
+        officeSectionWeekend: "Sat - Sun: days off",
+        officeSectionAddressTitle: "Address:",
+        officeSectionAddress: "Pl. Pobedy 25, office 201",
+        emailText:
+          "mailto:itext@itext.kz?subject=Ordering a translation from the website&body=Good afternoon! Translation of attached documents is required",
+        formTitle: "Translation Quote Form",
+        formParagraph:
+          "Fill out the form, and we will reply as soon as possible.",
+        formLabelName: "Your name (required)",
+        formLabelEmail: "Your email (required)",
+        formLabelPhone: "Your phone number (required)",
+        formLabelSourceLanguage: "Source language",
+        formLabelSourceLanguageSelect: "Select the source language",
+        formLabelTargetLanguage: "Target language",
+        formLabelTargetLanguageSelect: "Select the target language",
+        translationLanguages: [
+          "Kazakh",
+          "English",
+          "Russian",
+          "German",
+          "Bulgarian",
+          "Hungarian",
+          "Greek",
+          "Danish",
+          "Spanish",
+          "Italian",
+          "Latvian",
+          "Lithuanian",
+          "Dutch",
+          "Polish",
+          "Portuguese",
+          "Romanian",
+          "Slovak",
+          "Slovenian",
+          "Finnish",
+          "French",
+          "Czech",
+          "Swedish",
+          "Estonian",
+          "Turkish",
+          "Chinese",
+        ],
+        formLabelFilesSelect: "Select files to translate",
+        formLabelCertification: "I need a notarized translation",
+        formLabelCertificationText:
+          "The following conditions are required for notarization: 1) You need to bring the original document to our office at: 25, Pl. Pobedy, office 201, Pavlodar, Kazakhstan. 2) If the document was issued outside the CIS countries, it must have an apostille  (for example, the document was issued in Germany) or a consular legalization stamp (for example, the document was issued in China or the UAE).",
+        formLabelComment: "Your comment on the order",
+        buttonText: "Get a Quote",
+      },
+
       name: "",
       email: "",
       phone: "",
       message: "",
+      emailError: false,
+      phoneError: false,
+      nameError: false,
       errors: {
         name: "",
         email: "",
         phone: "",
-        sourceLanguage: "",
-        targetLanguage: "",
       },
     };
   },
+  watch: {
+    name: function (newValue, oldValue) {
+      if (newValue && !this.validateName(newValue)) {
+        this.nameError = true;
+      } else {
+        this.nameError = false;
+      }
+    },
+
+    email: function (newValue, oldValue) {
+      if (newValue && !this.validateEmail(newValue)) {
+        this.emailError = true;
+      } else {
+        this.emailError = false;
+      }
+    },
+
+    phone: function (newValue, oldValue) {
+      if (newValue && !this.validatePhone(newValue)) {
+        this.phoneError = true;
+      } else {
+        this.phoneError = false;
+      }
+    },
+  },
+
   methods: {
-    validateForm() {
-      this.errors = {
-        name: "",
-        email: "",
-        phone: "",
-        sourceLanguage: "",
-        targetLanguage: "",
-      };
-
-      if (this.validateName() && this.validateEmail()) {
-        console.log("Form is valid");
-        this.sendFormData();
-        return true;
-      } else {
-        console.log("Form is invalid");
-        console.log(this.errors);
-        console.log(Object.keys(this.errors).length);
-        return false;
-      }
-    },
-
-    // Validate name and console.log error message
-
+    //Validate name
     validateName() {
-      console.log(this.name);
-      if (!this.name) {
+      const regex = /^[a-zA-Zа-яА-Я]+$/;
+      if (!regex.test(this.name)) {
         if (getLanguage() === "ru") {
-          this.errors.name = "Поле имя обязательно для заполнения";
+          this.errors.name = "Имя должно содержать только буквы";
         } else if (getLanguage() === "en") {
-          this.errors.name = "The name field is required";
+          this.errors.name = "Name must contain only letters";
         } else if (getLanguage() === "kk") {
-          this.errors.name = "Атыңызды енгізіңіз";
+          this.errors.name = "Атыңыз тек әріптерден тұруы керек";
         }
         return false;
       } else {
-        // Set error to empty string if name is valid and return true
         this.errors.name = "";
-        this.sendFormData();
-        return true;
       }
+
+      return true;
     },
 
+    //Validate email
     validateEmail() {
-      console.log(this.email);
-      if (!this.email) {
+      const regex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regex.test(this.email)) {
         if (getLanguage() === "ru") {
-          this.errors.email = "Поле email обязательно для заполнения";
+          this.errors.email =
+            "Адрес электронной почты должен содержать символ @ и доменное имя";
         } else if (getLanguage() === "en") {
-          this.errors.email = "The email field is required";
+          this.errors.email =
+            "The email address must contain the @ symbol and a domain name";
         } else if (getLanguage() === "kk") {
-          this.errors.email = "Email енгізіңіз";
+          this.errors.email =
+            "Электрондық пошта мекенжайында @ белгісі мен домен атауы болуы керек";
         }
       } else {
-        const regex =
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!regex.test(this.email)) {
-          if (getLanguage() === "ru") {
-            this.errors.email = "Неверный email";
-          } else if (getLanguage() === "en") {
-            this.errors.email = "Invalid email";
-          } else if (getLanguage() === "kk") {
-            this.errors.email = "Жарамсыз email";
-          }
-        } else {
-          console.log("Email is valid");
-          return true;
-        }
+        this.errors.email = "";
+        return true;
       }
       return false;
     },
 
+    //Validate phone
     validatePhone() {
-      console.log(this.phone);
-      if (!this.phone) {
+      const regex = /^[+]*[0-9]*$/;
+      if (!regex.test(this.phone)) {
         if (getLanguage() === "ru") {
-          this.errors.phone = "Поле телефон обязательно для заполнения";
+          this.errors.phone = "В номере телефона могут быть только цифры";
         } else if (getLanguage() === "en") {
-          this.errors.phone = "The phone field is required";
+          this.errors.phone = "The phone number can only contain numbers";
         } else if (getLanguage() === "kk") {
-          this.errors.phone = "Телефон енгізіңіз";
+          this.errors.phone = "Телефон нөмірінде тек сандар болуы мүмкін";
         }
+        return false;
       } else {
-        const regex = /^[+]*[0-9]*$/;
-        if (!regex.test(this.phone)) {
-          if (getLanguage() === "ru") {
-            this.errors.phone = "Неверный телефон";
-          } else if (getLanguage() === "en") {
-            this.errors.phone = "Invalid phone";
-          } else if (getLanguage() === "kk") {
-            this.errors.phone = "Жарамсыз телефон";
-          }
-        } else {
-          console.log("Phone is valid");
-        }
+        this.errors.phone = "";
       }
-    },
 
-    validateSourceLanguage() {
-      const sourceLanguage = this.$refs["source-language"].value;
-      console.log(sourceLanguage);
-      if (sourceLanguage === "") {
-        if (getLanguage() === "ru") {
-          this.errors.sourceLanguage = "Выберите язык оригинала";
-        } else if (getLanguage() === "en") {
-          this.errors.sourceLanguage = "Please select a source language";
-        } else if (getLanguage() === "kk") {
-          this.errors.sourceLanguage = "Бастапқы тілді таңдаңыз";
-        }
-      } else {
-        console.log("Source language is valid");
-        // this.errors.sourceLanguage = "";
-      }
-    },
-
-    validateTargetLanguage() {
-      const targetLanguage = this.$refs["target-language"].value;
-      console.log(targetLanguage);
-      if (targetLanguage === "") {
-        if (getLanguage() === "ru") {
-          this.errors.targetLanguage = "Выберите язык перевода";
-        } else if (getLanguage() === "en") {
-          this.errors.targetLanguage = "Please select a target language";
-        } else if (getLanguage() === "kk") {
-          this.errors.targetLanguage = "Аяқталатын тілді таңдаңыз";
-        }
-      } else {
-        console.log("Target language is valid");
-        // this.errors.targetLanguage = "";
-      }
-    },
-
-    clearErrors() {
-      Object.keys(this.errors).forEach((key) => {
-        this.errors[key] = "";
-      });
-    },
-
-    async sendFormData() {
-      try {
-        const form = document.getElementById("my-form");
-        const data = new FormData(form);
-        const response = await fetch(form.action, {
-          method: form.method,
-          body: data,
-          headers: {
-            Accept: "application/json",
-          },
-        });
-        const status = document.getElementById("my-form-status");
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset();
-        } else {
-          const data = await response.json();
-          if (Object.hasOwn(data, "errors")) {
-            status.innerHTML = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerHTML = "Oops! There was a problem submitting your form";
-          }
-        }
-      } catch (error) {
-        const status = document.getElementById("my-form-status");
-        status.innerHTML = "Oops! There was a problem submitting your form";
-      }
+      return true;
     },
   },
+
   computed: {
     language() {
       switch (getLanguage()) {
